20cc5f85d3be5dbe07095d432559c223d34ef991
vovatran1993 <vovatran1993@gmail.com>
2019-12-08 16:35:14 -0600

    ec2

---
+++ ec2.tf
@@ -0,0 +1,15 @@
+module "ec2_cluster" {
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  version                = "~> 1.0"
+
+  name                   = "my-cluster"
+  instance_count         = 2
+
+  ami                    = "ami-00068cd7555f543d5"
+  instance_type          = "t2.micro"
+  key_name               = "vova_mac"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-d1b9e784"]
+  subnet_id              = "subnet-eddcdzz4"
+
+}
