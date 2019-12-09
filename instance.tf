797444fab8e6ae78ec1f2c7248bcfcf16e467d04
vovatran1993 <vovatran1993@gmail.com>
2019-12-08 18:21:52 -0600

    sss

--- instance.tf
+++ instance.tf
@@ -2,4 +2,5 @@ resource "aws_instance" "web" {
   ami           = "ami-00068cd7555f543d5"
   instance_type = "t2.micro"
   user_data     = "${file("user_data.sh")}"
+  vpc_security_group_ids = "${aws_security_group.web.id}"
 }
